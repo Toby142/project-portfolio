e13b12bcffb5debc1c4048b699f1ade7f81479d7
Toby142 <tobiasvandeven142@gmail.com>
2023-11-30 14:15:42 +0100

    page transition animation

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -1,51 +1,56 @@
 import {
-    trigger,
-    animate,
-    transition,
-    style,
-    query
-  } from '@angular/animations';
-  
-  export const fadeAnimation = trigger('fadeAnimation', [
-    transition('* => *', [
-      query(
-        ':enter',
-        [
-          style({
-            opacity: 0,
-            // position: 'absolute',
-            // height: '100%',
-            // width: '100%'
-          })
-        ],
-        { optional: true }
-      ),
-      query(
-        ':leave',
-        // here we apply a style and use the animate function to apply the style over 0.3 seconds
-        [
-          style({
-            opacity: 1,
-            position: 'absolute',
-            height: '100%',
-            width: '100%'
-          }),
-          animate('.2s', style({ opacity: 0 }))
-        ],
-        { optional: true }
-      ),
-      query(
-        ':enter',
-        [
-          style({
-            opacity: 0,
-            // position: 'relative',
-            // height: '100%',
-            // width: '100%'
-          }),
-          animate('.3s', style({ opacity: 1 }))
-        ],
-        { optional: true }
-      )
-    ])
-  ]);
+  trigger,
+  animate,
+  transition,
+  style,
+  query,
+  animateChild
+} from '@angular/animations';
+
+export const fadeAnimation = trigger('fadeAnimation', [
+  transition('* => *', [
+    query(
+      ':enter',
+      [
+        style({
+          opacity: 0,
+          backgroundColor: 'white',
+          position: 'absolute',
+          height: '100%',
+          width: '100%'
+        })
+      ],
+      { optional: true }
+    ),
+    query(
+      ':leave',
+      [
+        style({
+          opacity: 1,
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+          backgroundColor: 'white'
+        }),
+        animate('0.2s', style({ opacity: 0 })),
+        animateChild() // Animate child elements during the transition
+      ],
+      { optional: true }
+    ),
+    query(
+      ':enter',
+      [
+        style({
+          opacity: 0,
+          backgroundColor: 'transparent',
+          position: 'relative',
+          height: '100%',
+          width: '100%'
+        }),
+        animate('0.3s', style({ opacity: 1 })),
+        animateChild() // Animate child elements during the transition
+      ],
+      { optional: true }
+    )
+  ])
+]);
